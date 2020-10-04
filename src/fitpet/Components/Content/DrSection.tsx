5ecaff4437a199914d75a32da14400fde1249eaa
jihoon.moon <mjjjhs@gmail.com>
2020-10-05 00:41:37 +0900

    소스 정리

--- src/fitpet/Components/Content/DrSection.tsx
+++ src/fitpet/Components/Content/DrSection.tsx
@@ -1,46 +1,41 @@
 import * as React from 'react';
 import {DrSectionStyle} from "../../Styles";
 import {createRef, LegacyRef, SyntheticEvent, useEffect} from "react";
-import {isMobile, isSafari, getUA} from 'react-device-detect';
+// import {isMobile, isSafari, getUA} from 'react-device-detect';
 
 function DrSection({
                      setDrSectionHeight,
                      clientWidth
 }: {
-  setDrSectionHeight: (height:number) => void,
-  clientWidth: number
+  setDrSectionHeight?: (height:number) => void,
+  clientWidth?: number
 }) {
   const drSection: LegacyRef<HTMLDivElement> = createRef();
+  //
+  // useEffect(() => {
+  //   if(!drSection?.current?.offsetHeight) {
+  //     return;
+  //   }
+  //   setDrSectionHeight(drSection.current.offsetHeight);
+  // }, [clientWidth]);
 
-  useEffect(() => {
-    if(!drSection?.current?.offsetHeight) {
-      return;
-    }
-    setDrSectionHeight(drSection.current.offsetHeight);
-  }, [clientWidth]);
-
-  const onImgLoad = (e: SyntheticEvent): void => {
+  /*const onImgLoad = (e: SyntheticEvent): void => {
     if(isMobile) {
       if(isSafari) {
-        alert(1);
         setDrSectionHeight(e.currentTarget.clientHeight + 40);
       } else if(getUA.includes('KAKAOTALK')) {
-        alert(5);
         setDrSectionHeight(e.currentTarget.clientHeight - 110);
       } else {
-        alert(2);
         setDrSectionHeight(e.currentTarget.clientHeight + 150 );
       }
     } else {
       if(isSafari) {
-        alert(3);
         setDrSectionHeight(e.currentTarget.clientHeight + 40);
       } else {
-        alert(4);
         setDrSectionHeight(e.currentTarget.clientHeight + 110);
       }
     }
-  };
+  };*/
 
   return (
     <React.Fragment>
@@ -51,7 +46,7 @@ function DrSection({
              sizes="(max-width: 319px) 192px,
                     (max-width: 767px) 256px,
                     320px"
-             onLoad={onImgLoad}
+             // onLoad={onImgLoad}
         />
       </section>
       <style jsx>{DrSectionStyle}</style>
